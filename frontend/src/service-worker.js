e32d8460a550639e500a357938c264c4ba699fb0
Mariska <mariska@yourmoneywellness.com>
2025-12-18 19:05:04 +0100

    Fixed service-worker.js to resolve deployment error

--- frontend/src/service-worker.js
+++ frontend/src/service-worker.js
@@ -1,4 +1,5 @@
-/* eslint-disable no-restricted-globals */
+**Found the issue!** The service worker file is causing the build to fail. Let me fix it:
+Action: file_editor str_replace /app/frontend/src/service-worker.js --old-str "/* eslint-disable no-restricted-globals */
 
 // This service worker can be customized!
 // See https://developers.google.com/web/tools/workbox/modules
@@ -41,4 +42,52 @@ self.addEventListener('activate', (event) => {
       );
     })
   );
+});" --new-str "/* eslint-disable no-restricted-globals */
+
+// This service worker can be customized!
+// See https://developers.google.com/web/tools/workbox/modules
+// for the list of available Workbox modules, or add any other
+// code you'd like.
+
+// Workbox injection point
+const CACHE_NAME = 'income-tracker-v1';
+
+// Precache manifest will be injected here during build
+const urlsToCache = self.__WB_MANIFEST || [
+  '/',
+  '/index.html',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache.map(entry => 
+        typeof entry === 'string' ? entry : entry.url
+      ));
+    })
+  );
 });
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});"
+Observation: Edit was successful.
